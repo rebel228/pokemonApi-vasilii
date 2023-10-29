f204fad84af735bd1fe0cb1930c9509da08a9389
RichardPickman <drezzerock@gmail.com>
2023-10-29 18:20:15 +0200

    feat: utilize sections in index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
+import './App.css';
+import ErrorBoundary from './components/Error';
+import ErrorController from './components/ErrorController';
+import Search from './components/Search';
+import { Pokemon } from './types';
+import { fetchPokemons } from './utils/pokemons';
+import Pokemons from './components/Pokemons';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Props {}
+interface State {
+    query: string;
+    pokemons: Pokemon[];
+    isLoading: boolean;
 }
 
-export default App
+class App extends Component<Props, State> {
+    state: State = {
+        query: '',
+        pokemons: [],
+        isLoading: false,
+    };
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    getPokemons = (query: string) => {
+        this.setState((state) => ({ ...state, isLoading: true }));
+
+        fetchPokemons(query).then((pokemons) =>
+            this.setState(() => ({ pokemons, isLoading: false }))
+        );
+    };
+
+    render() {
+        return (
+            <ErrorBoundary>
+                <main>
+                    <ErrorController />
+                    <Search onClick={this.getPokemons} />
+                    {this.state.isLoading ? (
+                        <div className="loading" />
+                    ) : (
+                        <Pokemons pokemons={this.state.pokemons} />
+                    )}
+                </main>
+            </ErrorBoundary>
+        );
+    }
+}
+
+export default App;
