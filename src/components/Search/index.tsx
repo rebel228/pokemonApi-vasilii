392680b054e2ed9abc4a3a47acf725cbb274b740
RichardPickman <drezzerock@gmail.com>
2023-10-29 18:17:59 +0200

    feat: utilize components and add localstorage logic

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+import Button from '../Button';
+import Input from '../Input';
+
+interface Props {
+    onClick: (input: string) => void;
+}
+
+interface State {
+    query: string;
+}
+
+export default class Search extends Component<Props, State> {
+    state: State = {
+        query: localStorage.getItem('history') || '',
+    };
+
+    saveToHistory = () => {
+        const query = this.state.query;
+
+        localStorage.setItem('history', query);
+    };
+
+    handleClick = () => {
+        this.props.onClick(this.state.query);
+
+        this.saveToHistory();
+    };
+
+    render() {
+        return (
+            <section className="search">
+                <div>
+                    <Input
+                        onChange={(query) =>
+                            this.setState(() => ({ ...this.state, query }))
+                        }
+                        value={this.state.query || ''}
+                    />
+                    <Button onClick={this.handleClick} />
+                </div>
+            </section>
+        );
+    }
+}
