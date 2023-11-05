e41c79eb1a1cc939d8c6106415ca7986005b91ae
RichardPickman <drezzerock@gmail.com>
2023-11-05 23:24:30 +0200

    feat; add single pokemon card

---
+++ src/components/SinglePokemon/index.tsx
@@ -0,0 +1,20 @@
+import { useLoaderData } from 'react-router-dom';
+import { Pokemon } from '../../types';
+
+export const SinglePokemon = () => {
+    const { item } = useLoaderData() as { item: Pokemon };
+
+    console.log(item);
+
+    return (
+        <div className="pokemon">
+            <div className="pokemon__media">
+                <img src={item.image} alt="Pokemon image" />
+            </div>
+            <div className="pokemon__info">
+                <p>{item.name}</p>
+                <p>{item.description}</p>
+            </div>
+        </div>
+    );
+};
