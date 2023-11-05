9be2decfcfbe5b899059f666fd97b52f63ded6ce
RichardPickman <drezzerock@gmail.com>
2023-11-05 23:24:20 +0200

    feat: add styles for search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,10 +3,12 @@ import Button from '../Button';
 import Input from '../Input';
 
 interface Props {
+    limit: number;
     onClick: (input: string) => void;
+    onLimitChange: (limit: number) => void;
 }
 
-const Search = ({ onClick }: Props) => {
+const Search = ({ limit, onClick, onLimitChange }: Props) => {
     const [query, setQuery] = useState('');
 
     const handleClick = () => {
@@ -17,9 +19,38 @@ const Search = ({ onClick }: Props) => {
     const saveToHistory = () => localStorage.setItem('history', query);
 
     return (
-        <section className="search">
-            <Input onChange={setQuery} value={query || ''} />
-            <Button onClick={handleClick} />
+        <section
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    justifyContent: 'center',
+                }}
+            >
+                <Input onChange={setQuery} value={query || ''} />
+                <Button onClick={handleClick}>Search</Button>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    justifyContent: 'center',
+                }}
+            >
+                <label>Limit: </label>
+                <input
+                    type="number"
+                    value={limit}
+                    onChange={(event) =>
+                        onLimitChange(Number(event.target.value))
+                    }
+                />
+            </div>
         </section>
     );
 };
