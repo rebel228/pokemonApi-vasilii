fd93b98493c5cffad028b9ee567ff0fefeb78b0d
RichardPickman <drezzerock@gmail.com>
2023-11-05 23:24:12 +0200

    feat: update pokemon card

--- src/components/Pokemons/index.tsx
+++ src/components/Pokemons/index.tsx
@@ -2,13 +2,18 @@ import { Pokemon } from '../../types';
 
 interface Props {
     pokemons: Pokemon[];
+    onClick: (poke: string) => void;
 }
 
-const Pokemons = ({ pokemons }: Props) => {
+const Pokemons = ({ pokemons, onClick }: Props) => {
     return (
         <section className="pokemon_list">
             {pokemons.map((item) => (
-                <div key={item.name + item.description} className="pokemon">
+                <div
+                    key={item.name + item.description}
+                    className="pokemon"
+                    onClick={() => onClick(item.name)}
+                >
                     <div className="pokemon__media">
                         <img src={item.image} alt="Pokemon image" />
                     </div>
